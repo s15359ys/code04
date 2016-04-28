a1464774007eae72c6bf0413275d374706733a23
s15359ys <s15359ys@sfc.keio.ac.jp>
2016-04-28 12:30:42 +0900

    関数exorを作成
    関数exor：入力された二つの引数の真偽を比較し、真偽が一致しない時にtrueを出力する関数

--- js/logic.js
+++ js/logic.js
@@ -1,6 +1,12 @@
 function imply(a,b){
-var result = !a||a&&b;
-return result;}
+    var result = !a||a&&b;
+    return result;}
 function nand(a,b){
-var result = !(a&&b);
-return result;}
+    var result = !(a&&b);
+    return result;}
+function nor(a,b){
+    var result = !a&&!b;
+    return result;}
+function exor(a,b){
+    var result = !(a==b);
+    return result;}
